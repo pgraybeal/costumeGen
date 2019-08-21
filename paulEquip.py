a42d1246b19f84323ddff9f3cbd8aff864efbefc
Paul Graybeal <Paul@Pauls-MacBook-Pro-2.local>
2019-08-21 13:52:35 -0700

    Updated 20190821 1:52pm

--- paulEquip.py
+++ paulEquip.py
@@ -1,14 +1,15 @@
 import osrsbox
 from jsonExtract import importPickle, statsLookup
 
+
 coordinates = {}  # dictionary containing the image coordinates (on the equip tab) of each type of item
-stats = {}
-itemSlots = {}
 image = 0
 # IMPORTANT: before this code, you would create variables 'stats' and 'itemSlots', and then import the pickle files
+slot = 0
+pp = pprint.PrettyPrinter(indent=4)
 
 
-class EquipmentView:
+class Equipment:
     def __init__(
         self,
         image,
@@ -39,31 +40,36 @@ class EquipmentView:
         self.weapon = weapon
         self.shield = shield
 
-        equippedItems = {'2h': self.twoHand,  # this Dict will be handy for the equip and statLookup functions
-                         'helmet': self.helmet,
-                         'amulet': self.amulet,
-                         'ring': self.ring,
-                         'body': self.body,
-                         'legs': self.legs,
-                         'gloves': self.gloves,
-                         'boots': self.boots,
-                         'cape': self.cape,
-                         'ammo': self.ammo,
-                         'weapon': self.weapon,
-                         'shield': self.shield,
-                         }
+        self.stats = importPickle('stats.pickle')
+        self.itemSlots = importPickle('itemSlots.pickle')
+        self.slot = 0
+
+        self.equippedItems = {'2h': self.twoHand,  # this Dict will be handy for the equip and statLookup functions
+                              'helmet': self.helmet,
+                              'amulet': self.amulet,
+                              'ring': self.ring,
+                              'body': self.body,
+                              'legs': self.legs,
+                              'gloves': self.gloves,
+                              'boots': self.boots,
+                              'cape': self.cape,
+                              'ammo': self.ammo,
+                              'weapon': self.weapon,
+                              'shield': self.shield,
+                              }
 
     def equip(self, itemName):
-        slot = itemSlots[itemName.lower()]  # Getting a key error here??? How??
-        image = item.get_image()
+        self.slot = self.itemSlots[itemName.lower()]  # Getting a key error here??? How??
+        #image = item.get_image()
+
         # based on what the slot is, replace the current item with that item
         # then, using coordinates, put the item on the equipment image
 
-        if equippedItems[slot] == None:
-            equippedItems[slot] = itemName.lower()
+        if self.equippedItems[self.slot] == None:
+            self.equippedItems[self.slot] = itemName.lower()
 
-        if equippedItems[slot] != None:
-            equippedItems[slot] = itemName.lower()
+        elif self.equippedItems[self.itemSlots[itemName.lower()]] != None:
+            self.equippedItems[self.itemSlots[itemName.lower()]] = itemName.lower()
             getAllStats()
             # IMPORTANT: this will replace anything equipped, but if we replace an equipped item, we need to rerun statsLookup
 
@@ -91,13 +97,15 @@ class EquipmentView:
         # get image from osrsdb using self.id
         raise NotImplementedError
 
+    def printEquipped(self):
+        for k, v in self.equippedItems.items():
+            print(f'{k}: {v}')
+
 
 def main():
-    stats = importPickle('stats.pickle')
-    itemSlots = importPickle('itemSlots.pickle')
-
-    instance = EquipmentView(image)
+    instance = Equipment(image)
     instance.equip('maple longbow')
+    instance.printEquipped()
 
 
 if __name__ == "__main__":
